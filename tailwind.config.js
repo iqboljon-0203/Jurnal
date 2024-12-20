4ba22fd4a895c911a67a72e3df6308929d6cf8b4
Iqboljon <iqboljon2003.yuldashev@gmail.com>
2024-12-20 11:00:01 +0500

    new add

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
