38fcb1c2e0b90d11f7ff5220cfd07474277060c2
Iqboljon <iqboljon2003.yuldashev@gmail.com>
2024-12-20 11:15:24 +0500

    a changed link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,41 +1,42 @@
 import { ChevronDown } from 'lucide-react';
+import {Link} from "react-router-dom";
 export default function NavItem({ text, hasDropdown = false }){
     return (
       <div className="relative group">
         <button className="flex items-center gap-1 text-white/90 hover:text-white py-6">
-          <a href={text==="Maqolalar"?"/article":text==="Bog'lanish"?"/contact":text==="Arxiv"?"/archive":"/"}>
+          <Link to={text==="Maqolalar"?"/article":text==="Bog'lanish"?"/contact":text==="Arxiv"?"/archive":"/"}>
           <span className='font-manrope text-base font-normal leading-[1.37rem] text-left text-white hover:text-[#FFC82A]'>{text}</span>
-          </a>
+          </Link>
           {hasDropdown && <ChevronDown className="h-4 w-4 group-hover:rotate-180 transition-transform" />}
         </button>
         
         {hasDropdown==="about" ? (
           <div className="absolute top-full left-0 w-48 bg-white rounded-md shadow-lg py-2 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all">
-            <a href="/journal" className="block px-4 py-2 font-manrope text-base font-normal leading-[1.37rem] text-left  hover:text-[#FFC82A]">
+            <Link to="/journal" className="block px-4 py-2 font-manrope text-base font-normal leading-[1.37rem] text-left  hover:text-[#FFC82A]">
               Jurnal haqida
-            </a>
-            <a href="/plans" className="block px-4 py-2 font-manrope text-base font-normal leading-[1.37rem] text-left  hover:text-[#FFC82A]">
+            </Link>
+            <Link to="/plans" className="block px-4 py-2 font-manrope text-base font-normal leading-[1.37rem] text-left  hover:text-[#FFC82A]">
               Rejalar va vazifalar
-            </a>
-            <a href="/editorial" className="block px-4 py-2 font-manrope text-base font-normal leading-[1.37rem] text-left  hover:text-[#FFC82A]">
+            </Link>
+            <Link to="/editorial" className="block px-4 py-2 font-manrope text-base font-normal leading-[1.37rem] text-left  hover:text-[#FFC82A]">
               Tahririyat hay'ati
-            </a>
-            <a href="/reviewers" className="block px-4 py-2 font-manrope text-base font-normal leading-[1.37rem] text-left  hover:text-[#FFC82A]">
+            </Link>
+            <Link to="/reviewers" className="block px-4 py-2 font-manrope text-base font-normal leading-[1.37rem] text-left  hover:text-[#FFC82A]">
               Taqrizchilar kengashi
-            </a>
+            </Link>
             
           </div>
         ): hasDropdown==="authors" ? (
           <div className="absolute top-full left-0 w-48 bg-white rounded-md shadow-lg py-2 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all">
-            <a href="/guidelines" className="block px-4 py-2 font-manrope text-base font-normal leading-[1.37rem] text-left  hover:text-[#FFC82A]">
+            <Link to="/guidelines" className="block px-4 py-2 font-manrope text-base font-normal leading-[1.37rem] text-left  hover:text-[#FFC82A]">
               Yo'riqnomalar
-            </a>
-            <a href="/send_article" className="block px-4 py-2 font-manrope text-base font-normal leading-[1.37rem] text-left  hover:text-[#FFC82A]">
+            </Link>
+            <Link to="/send_article" className="block px-4 py-2 font-manrope text-base font-normal leading-[1.37rem] text-left  hover:text-[#FFC82A]">
               Maqola yuborish
-            </a>
-            <a href="/checklist" className="block px-4 py-2 font-manrope text-base font-normal leading-[1.37rem] text-left  hover:text-[#FFC82A]">
+            </Link>
+            <Link to="/checklist" className="block px-4 py-2 font-manrope text-base font-normal leading-[1.37rem] text-left  hover:text-[#FFC82A]">
               Tekshirish ro'yxati
-            </a>
+            </Link>
             
           </div>
         ):false}
